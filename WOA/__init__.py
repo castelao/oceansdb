f90dfb4526fda640e5316ee7656d0cdb503b3ca7
Gui <guilherme@castelao.net>
2015-12-18 10:51:21 -0800

    Missing __init__.py

---
+++ WOA/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Guilherme Castelao'
+__email__ = 'guilherme@castelao.net'
+__version__ = '0.0.1'
+
+from WOA.woa import woa_profile
+from WOA.woa import woa_profile_from_dap
+from WOA.woa import woa_profile_from_file
+from WOA.woa import woa_track_from_file
+#from WOA.woa import build_input
+from WOA.woa import woa_from_file
