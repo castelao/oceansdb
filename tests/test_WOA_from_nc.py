8618c68046487d475c077cb30070c9080cc4fbc7
Gui <guilherme@castelao.net>
2015-12-28 01:49:27 -0800

    Test prototype for WOA from a netCDF file.

---
+++ tests/test_WOA_from_nc.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+"""
+
+from datetime import datetime
+
+from WOA.woa import WOA
+
+def test_import():
+    # A shortcut
+    from WOA import WOA
+    db = WOA()
+
+
+def test_available_vars():
+    db = WOA()
+    
+    for v in ['TEMP', 'PSAL']:
+        assert v in db.keys()
+
+      
+def test_get_profile():
+    db = WOA()
+
+    db['TEMP'].get_profile(var='mn', doy=datetime.now(),
+            depth=0, lat=10, lon=330)
+    db['TEMP'].get_profile(var='mn', doy=datetime.now(),
+            depth=[0,10], lat=10, lon=330)
+    db['TEMP'].get_profile(doy=datetime.now(),
+            depth=[0,10], lat=10, lon=330)
