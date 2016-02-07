c515ce6c0e3be1b638d4f736d200764db5801e40
Gui <guilherme@castelao.net>
2016-02-06 16:48:29 -0800

    Removing alias to old functions.

--- WOA/__init__.py
+++ WOA/__init__.py
@@ -6,9 +6,9 @@ __version__ = '0.0.1'
 
 
 from WOA.woa import WOA
-from WOA.woa import woa_profile
-from WOA.woa import woa_profile_from_dap
-from WOA.woa import woa_profile_from_file
-from WOA.woa import woa_track_from_file
+#from WOA.woa import woa_profile
+#from WOA.woa import woa_profile_from_dap
+#from WOA.woa import woa_profile_from_file
+#from WOA.woa import woa_track_from_file
 #from WOA.woa import build_input
-from WOA.woa import woa_from_file
+#from WOA.woa import woa_from_file
