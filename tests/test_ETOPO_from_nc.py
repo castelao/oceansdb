bf72cb66338355b07a817e8051b9371740556971
Guilherme Castelao <guilherme@castelao.net>
2016-12-17 14:48:36 -0800

    Updating ETOPO test.

--- tests/test_ETOPO_from_nc.py
+++ tests/test_ETOPO_from_nc.py
@@ -28,35 +28,35 @@ def test_available_vars():
 def test_coincident_gridpoint():
     db = ETOPO()
 
-    h = db.extract(lat=17.5, lon=0)
-    assert np.allclose(h['elevation'], [305.])
+    h = db['elevation'].extract(lat=17.5, lon=0)
+    assert np.allclose(h['elevation'], [298.])
 
 
-    h = db.extract(lat=[17.5, 18.5], lon=0)
-    assert np.allclose(h['elevation'], [305., 335.])
+    h = db['elevation'].extract(lat=[17.5, 18.5], lon=0)
+    assert np.allclose(h['elevation'], [298., 376.])
 
-    h = db.extract(lat=17.5, lon=[359.92, 0])
-    assert np.allclose(h['elevation'], [305., 305.])
+    h = db['elevation'].extract(lat=17.5, lon=[0, 0.25])
+    assert np.allclose(h['elevation'], [298., 310.])
 
-    h = db.extract(lat=[17.5, 18.5], lon=[-0.08, 0])
-    assert np.allclose(h['elevation'], [[305., 305.], [335., 335.]])
+    h = db['elevation'].extract(lat=[17.5, 18.5], lon=[0, 0.25])
+    assert np.allclose(h['elevation'], [[298., 310.], [376., 382.]])
 
 
 def test_lon_cyclic():
     db = ETOPO()
 
-    h1 = db.extract(lat=17.5, lon=182.5)
-    h2 = db.extract(lat=17.5, lon=-177.5)
+    h1 = db['elevation'].extract(lat=17.5, lon=182.5)
+    h2 = db['elevation'].extract(lat=17.5, lon=-177.5)
     assert np.allclose(h1['elevation'], h2['elevation'])
 
-    h1 = db.extract(lat=17.5, lon=[-37.5, -32.5])
-    h2 = db.extract(lat=17.5, lon=[322.5, 327.5])
+    h1 = db['elevation'].extract(lat=17.5, lon=[-37.5, -32.5])
+    h2 = db['elevation'].extract(lat=17.5, lon=[322.5, 327.5])
     assert np.allclose(h1['elevation'], h2['elevation'])
 
     lons = 360 * np.random.random(10)
     for lon1 in lons:
-        h1 = db.extract(lat=17.5, lon=lon1)
+        h1 = db['elevation'].extract(lat=17.5, lon=lon1)
         lon2 = lon1 - 360
-        h2 = db.extract(lat=17.5, lon=lon2)
+        h2 = db['elevation'].extract(lat=17.5, lon=lon2)
         assert np.allclose(h1['elevation'], h2['elevation']), \
                 "Different elevation between: %s and %s" % (lon1, lon2)
