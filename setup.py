692e72c779db7c16c93e1670898003ecd2808713
Guilherme Castelao <castelao@hatch.ucsd.edu>
2017-05-13 22:47:09 -0700

    Metadata for compatibility with Python-3.6.

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Scientific/Engineering',
     ]
 )
