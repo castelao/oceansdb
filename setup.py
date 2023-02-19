21737f7d5377b28312e67c5972dd5cdbc2a6340b
Gui Castelao <guilherme@castelao.net>
2023-02-19 14:00:27 -0700

    Cleaning further metadata

--- setup.py
+++ setup.py
@@ -4,15 +4,10 @@
 
 
 from setuptools import setup
-from codecs import open
 
 setup(
-    author="Guilherme Castelao",
-    author_email='guilherme@castelao.net',
-    url='https://github.com/castelao/oceansdb',
     packages=['oceansdb'],
     package_dir={'oceansdb': 'oceansdb'},
     include_package_data=True,
-    license='3-clause BSD',
     zip_safe=False,
 )
