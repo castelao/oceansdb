22b1469d5b6433f293e10a464e7edef083cab395
Chris Slater <6004777+electricsam@users.noreply.github.com>
2023-02-21 06:44:11 -0700

    revert version bump

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open('requirements.txt', encoding='utf-8') as f:
 
 setup(
     name='oceansdb',
-    version='0.8.15',
+    version='0.8.14',
     description="Package to subsample ocean climatologies and reference data.",
     long_description=readme + '\n\n' + history,
     author="Guilherme Castelao",
